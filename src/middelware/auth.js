4ad1f6cac6adf375baea132ab0c42e45a887b929
Agustin <agustinescalanterojo@gmail.com>
2023-07-12 00:38:09 +0200

    feat: implement middelware

---
+++ src/middelware/auth.js
@@ -0,0 +1,54 @@
+import jwt from 'jsonwebtoken'
+import User from '../models/user.js'
+
+const publicUrls = ['/auth/login', '/auth/signup']
+
+/**
+ * Middleware para asegurar la autenticación del usuario.
+ *
+ * @param {object} request
+ * @param {object} response
+ * @param {function} next
+ */
+export const ensureAuthenticated = async (request, response, next) => {
+  // Comprobar si la ruta de la solicitud incluye '/auth'.
+  // Si es así, se permite el acceso sin autenticación.
+  if (publicUrls.includes(request.path)) {
+    return next()
+  }
+
+  // Comprobar si no se proporciona una cabecera 'Authorization' en la solicitud.
+  if (!request.headers.authorization) {
+    return response.status(403).send({ message: 'You are not authenticated 1' })
+  }
+
+  // Obtener el token de autorización de la cabecera 'Authorization'.
+  const token = request.headers.authorization.split(' ')[1]
+
+  // Comprobar si no se proporciona un token.
+  if (!token) {
+    return response.status(403).send({ message: 'You are not authenticated' })
+  }
+
+  // Decodificar el token JWT utilizando el secreto.
+  const payload = jwt.decode(token, process.env.TOKEN_SECRET)
+
+  // Comprobar si el token no es válido o no contiene un 'id' en el payload.
+  if (!payload || !payload.id) {
+    return response.status(403).send({ message: 'Wrong token' })
+  }
+
+  // Buscar al usuario en la base de datos utilizando el 'id' del payload.
+  const user = await User.findOne({ _id: payload.id })
+
+  // Comprobar si no se encuentra al usuario en la base de datos.
+  if (!user) {
+    return response.status(403).send({ message: 'Wrong token' })
+  }
+
+  // Agregar el usuario a la solicitud para que esté disponible en los controladores posteriores.
+  request.user = user
+
+  // Pasar la solicitud al siguiente middleware.
+  next()
+}
