f165ce178ddb924cc7b7348d4bc0ce15bbee486f
Agustin <agustinescalanterojo@gmail.com>
2023-07-19 19:23:32 +0200

    fix: refactor index

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import postRouter from './src/router/post.js'
 import authRouter from './src/router/auth.js'
+import adminRouter from './src/router/user.js'
+import commentsRouter from './src/router/comments.js'
+import favsRouter from './src/router/favs.js'
 import connectToDb from './src/services/db.js'
 import dotenv from 'dotenv'
 import { ensureAuthenticated } from './src/middelware/auth.js'
@@ -11,7 +14,7 @@ dotenv.config()
 
 const startApp = async () => {
   const app = express()
-  const port = process.env.PORT 
+  const port = process.env.PORT
 
   app.use(cors())
   app.use(bodyParser.json())
@@ -23,17 +26,16 @@ const startApp = async () => {
 
   app.use(ensureAuthenticated)
 
-  app.get('/', (request, response) => {
-    response.json({ info: 'hola mundo' })
-  })
-
   app.use('/posts', postRouter)
   app.use('/auth', authRouter)
+  app.use('/admin', adminRouter)
+  app.use('/comments', commentsRouter)
+  app.use('/toggle', favsRouter)
 
   try {
     await connectToDb()
     app.listen(port, () => {
-        console.log(`Server start in ${port} port`)
+      console.log(`Server start in ${port} port`)
     })
   } catch (e) {
     console.log(e)
